2c40aa47636a9b675bee81bad8c400320aae726d
hidaya <hidaya.regragui@gmail.com>
2023-12-22 22:53:13 +0100

    add  comment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,5 @@
+# create a file in /tmp
+
 file { '/tmp/school':
   content =>'I love Puppet',
   mode    => '0744',
---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,3 @@
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+}
