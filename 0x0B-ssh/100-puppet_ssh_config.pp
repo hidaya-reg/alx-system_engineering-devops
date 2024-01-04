f975d7e7aec65c4dc0d4f1ff339fd4bfa96f527f
hidaya <hidaya.regragui@gmail.com>
2024-01-05 00:47:31 +0100

    task 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# make changes to config file
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+content =>"
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+}
