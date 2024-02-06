152d0dbb412f0b563ec66f28c812bf5800b0895b
hidaya <hidaya.regragui@gmail.com>
2024-02-06 11:25:40 +0100

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,20 @@
 # 1-install_a_package.pp
 
+# Define package resource for flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+# Ensure python3-pip is installed
 package { 'python3-pip':
   ensure => installed,
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+# Define package resource for Werkzeug
+package { 'werkzeug':
+  ensure   => '2.1.1',  # Update the version according to Flask's requirements
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
