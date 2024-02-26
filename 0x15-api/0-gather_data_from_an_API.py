8ad79c80cb385d827be3e9c7734f9376750350ba
hidaya <hidaya.regragui@gmail.com>
2024-02-26 15:14:33 +0100

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,17 +4,26 @@ import requests
 from sys import argv
 
 
-if __name__ == '__main__':
+def gather_data():
+    """ Gather data from an API """
+    if len(argv) < 2:
+        exit()
+
     user_id = argv[1]
     url = 'https://jsonplaceholder.typicode.com/'
+    todos_response = requests.get(f"{url}/todos?userId={user_id}")
+    todos_data = todos_response.json()
 
-    user = requests.get(url + 'users/{}'.format(user_id)).json()
-    todos = requests.get(url + 'todos', params={'userId': user_id}).json()
-
-    completed_tasks = [task for task in todos if task.get('completed')]
-
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get('name'), len(completed_tasks), len(todos)))
-
+    user_response = requests.get(f"{url}/users?id={user_id}")
+    user_data = user_response.json()
+    user_name = user_data[0]["name"]
+    completed_tasks = [task for task in todos_data if task["completed"]]
+    completed = len(completed_tasks)
+    total = len(todos_data)
+    print("Employee {} is done with tasks ({}/{}):".format(user_name, completed, total))
     for task in completed_tasks:
-        print("\t {}".format(task.get('title')))
+        print("\t{}".format(task['title']))
+
+
+if __name__ == '__main__':
+    gather_data()
