94de3d379c93511ec0c5982470cf5f8e6ed42e61
hidaya <hidaya.regragui@gmail.com>
2024-03-31 09:06:23 +0000

    ti

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__":
 
-    userId = sys.argv[1]
+    userId = int(sys.argv[1])
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(userId))
 
