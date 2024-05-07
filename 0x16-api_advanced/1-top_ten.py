6fe7ab81115b32215e33a087ae80a9726bfcd0b8
hidaya <hidaya.regragui@gmail.com>
2024-05-07 21:54:25 +0100

    api advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,9 +13,9 @@ def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'MyUserAgent'}
 
-    response = requests.get(url, headers=headers, allow_redirects=False )
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
+    if response.status_code >= 300:
         print("None")
         return
 
