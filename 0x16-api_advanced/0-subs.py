889bf0470fabd8299b26b9cb68c65b2ab080bb34
hidaya <hidaya.regragui@gmail.com>
2024-05-07 20:38:09 +0100

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,10 +18,8 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'MyBot/0.0.1'}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    if response.status_code >= 300:
         return 0
+return sub_info.json().get("data").get("subscribers")
