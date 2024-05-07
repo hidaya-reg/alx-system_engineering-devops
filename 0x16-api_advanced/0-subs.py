a78435526c5247ba00868a1b0ee09257e6a734ad
hidaya <hidaya.regragui@gmail.com>
2024-05-07 21:39:07 +0100

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,7 +16,7 @@ def number_of_subscribers(subreddit):
         Returns 0 if the subreddit is invalid.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'MyBot/0.0.1'}
+    headers = {'User-Agent': 'MyUserAgent'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
